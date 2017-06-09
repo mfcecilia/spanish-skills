d9418e94f81ae44968e744ecae2c1d0c313a4904
Mykayla Fernandes <mykayla.fernandes@gmail.com>
2017-06-08 22:38:01 -0600

    0004 - start learn button js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 //index.js
 
-function startLearn(){
-    document.getElementById("learn-btn")
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */ /*global define */
+
+var learnBtn = document.getElementById("learn-btn");
+var questionView = document.getElementById("question-view");
+
+function startLearn() {
+    'use strict';
+    console.log("inside startLearn function");
+    if (learnBtn.onclick()) {
+        questionView.style.display = "block";
+    } else {
+        console.log("learn button failed");
+    }
+    console.log("startLearn function successful");
 }
